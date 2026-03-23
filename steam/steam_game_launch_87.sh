2acb2e073cfcca7f866844abc8271f70eb564245
fr3dx <77107619+fr3dx@users.noreply.github.com>
2026-03-23 18:34:48 +0100

    Update and rename steam_game_launch_88.sh to steam_game_launch_87.sh

--- steam/steam_game_launch_87.sh
+++ steam/steam_game_launch_87.sh
@@ -1,22 +1,25 @@
-# Path of Exile 2 settings
-
 #!/bin/bash
 
+# -------- Cleanup function --------
 cleanup() {
     powerprofilesctl set power-saver
 }
 trap cleanup EXIT SIGINT SIGTERM
 
+# -------- CPU / Power --------
 powerprofilesctl set performance
 
+# -------- Launch game --------
 MANGOHUD=1 gamemoderun gamescope \
     -w 2560 -h 1440 -r 120 -o 120 -f \
     --adaptive-sync \
     --immediate-flips \
     --force-grab-cursor \
+    --rt \
+    --framerate-limit 87 \
     -- \
     env \
-    MANGOHUD_CONFIG="fps_limit=86,no_display" \
+    MANGOHUD_CONFIG="fps_limit=87,no_display" \
     DXVK_ASYNC=1 \
     ENABLE_LAYER_MESA_ANTI_LAG=1 \
     "$@"
