296bbeb9cffbccaef5082212438d416f980c391b
fr3dx <77107619+fr3dx@users.noreply.github.com>
2026-03-25 19:15:56 +0100

    Update game launch script to remove framerate limit
    Removed framerate limit option from game launch script.

--- steam/steam_game_launch_117.sh
+++ steam/steam_game_launch_117.sh
@@ -11,12 +11,11 @@ powerprofilesctl set performance
 
 # -------- Launch game --------
 MANGOHUD=1 gamemoderun gamescope \
-    -w 2560 -h 1440 -r 120 -o 120 -f \
+    -w 2560 -h 1440 -r 120 -f \
     --adaptive-sync \
     --immediate-flips \
     --force-grab-cursor \
     --rt \
-    --framerate-limit 117 \
     -- \
     env \
     MANGOHUD_CONFIG="fps_limit=117,no_display" \
