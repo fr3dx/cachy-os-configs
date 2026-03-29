1c552f0b05221e88216e00bca34a314780a6126a
fr3dx <77107619+fr3dx@users.noreply.github.com>
2026-03-29 14:39:12 +0200

    Refactor cleanup function and improve comments
    Removed unnecessary comments and adjusted the cleanup function.

--- steam/steam_game_launch_117.sh
+++ steam/steam_game_launch_117.sh
@@ -3,15 +3,20 @@
 # LAVD powersave default, switch to performance for this session
 
 cleanup() {
-    # Restore powersave profile and LAVD
+    # Restore powersave profile
     powerprofilesctl set power-saver
+
     sudo systemctl stop scx_lavd-performance.service
+    sudo systemctl stop scx_lavd-powersave.service
     sudo systemctl start scx_lavd-powersave.service
 }
+
 trap cleanup EXIT SIGINT SIGTERM
 
-# Enable performance profile and scheduler
+# Enable performance profile
 powerprofilesctl set performance
+
+sudo systemctl stop scx_lavd-performance.service
 sudo systemctl stop scx_lavd-powersave.service
 sudo systemctl start scx_lavd-performance.service
 
@@ -19,11 +24,9 @@ sudo systemctl start scx_lavd-performance.service
 gamescope \
     -w 2560 -h 1440 -r 120 -f \
     --adaptive-sync \
-    --immediate-flips \
     --force-grab-cursor \
-    --rt \
     -- \
-    gamemoderun env \
+    env \
     DXVK_FRAME_RATE=117 \
     MANGOHUD=1 \
     MANGOHUD_CONFIG="read_cfg" \
