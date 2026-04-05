034b12892975b164c03da8053797b32cee913830
fr3dx <77107619+fr3dx@users.noreply.github.com>
2026-04-05 17:48:20 +0200

    Update steam_game_launch_117.sh for performance settings
    Refactor power profile management for game launch.

--- steam/steam_game_launch_117.sh
+++ steam/steam_game_launch_117.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
-# LAVD powersave default, switch to performance for this session
+########################################
+# Fr3dx's default competitive settings #
+########################################
 
+# Power profile function
+switch_lavd() {
+    sudo systemctl stop scx_lavd-$1.service 2>/dev/null
+    sudo systemctl start scx_lavd-$2.service 2>/dev/null
+}
+
+# Closing game -> Switch to powersave profile
 cleanup() {
-    # Restore powersave profile
-    powerprofilesctl set power-saver
-
-    sudo systemctl stop scx_lavd-performance.service
-    sudo systemctl stop scx_lavd-powersave.service
-    sudo systemctl start scx_lavd-powersave.service
+    powerprofilesctl set power-saver 2>/dev/null
+    switch_lavd "performance" "powersave"
 }
 
 trap cleanup EXIT SIGINT SIGTERM
 
-# Enable performance profile
-powerprofilesctl set performance
+# Starting game -> Switch to performance profile
+powerprofilesctl set performance 2>/dev/null
+switch_lavd "powersave" "performance"
 
-sudo systemctl stop scx_lavd-performance.service
-sudo systemctl stop scx_lavd-powersave.service
-sudo systemctl start scx_lavd-performance.service
 
-# Launch Gamescope with Proton + DXVK + MangoHud
+# Launch Gamescope with MangoHud, limit fps
 gamescope \
     -w 2560 -h 1440 -r 120 -f \
     --adaptive-sync \
