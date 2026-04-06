5ea45da8e1f5b3ec705c0bf21d98a5a219f0e36f
fr3dx <77107619+fr3dx@users.noreply.github.com>
2026-04-06 06:18:49 +0200

    Update Gamescope launch parameters in script

--- steam/steam_game_launch_87.sh
+++ steam/steam_game_launch_87.sh
@@ -25,15 +25,12 @@ switch_lavd "powersave" "performance"
 
 # Launch Gamescope with MangoHud, limit fps
 gamescope \
-    -w 2560 -h 1440 -r 120 -o 120 -f \
+    -w 2560 -h 1440 -r 120 -f \
     --adaptive-sync \
-    --immediate-flips \
     --force-grab-cursor \
-    --rt \
-    --framerate-limit 87 \
     -- \
     env \
     MANGOHUD=1 \
-    MANGOHUD_CONFIG="fps_limit=87,no_display,af=16,bicubic" \
+    MANGOHUD_CONFIG="fps_limit=87,read_cfg,af=16" \
     DXVK_ASYNC=1 \
     "$@"
