8db6a2f7f254833d0243db8af25592e260ce579c
fr3dx <77107619+fr3dx@users.noreply.github.com>
2026-04-08 19:21:51 +0200

    Update comments for clarity in steam_game_launch script

--- steam/steam_game_launch_117.sh
+++ steam/steam_game_launch_117.sh
@@ -4,13 +4,13 @@
 # Fr3dx's default competitive settings #
 ########################################
 
-# Power profile function
+# LAVD scheduler mode function
 switch_lavd() {
     sudo systemctl stop scx_lavd-$1.service 2>/dev/null
     sudo systemctl start scx_lavd-$2.service 2>/dev/null
 }
 
-# Closing game -> Switch to powersave profile
+# Closing game -> Switch to powersave mode
 cleanup() {
     powerprofilesctl set power-saver 2>/dev/null
     switch_lavd "performance" "powersave"
@@ -18,19 +18,18 @@ cleanup() {
 
 trap cleanup EXIT SIGINT SIGTERM
 
-# Starting game -> Switch to performance profile
+# Starting game -> Switch to performance mode
 powerprofilesctl set performance 2>/dev/null
 switch_lavd "powersave" "performance"
 
 
-# Launch Gamescope with MangoHud, limit fps
+# Launch Gamescope with MangoHud. Do not forget limit fps ingame -> 115,116,119!
 gamescope \
     -w 2560 -h 1440 -r 120 -f \
     --adaptive-sync \
     --force-grab-cursor \
     -- \
     env \
-    VKD3D_FRAME_RATE=117 \
     MANGOHUD=1 \
     MANGOHUD_CONFIG="read_cfg" \
     "$@"
